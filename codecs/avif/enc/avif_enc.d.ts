83f275a9cadcf2bde08128c1b3c9f60fab712192
Mr.Control <phucuong13029x@gmail.com>
2021-03-16 14:44:06 +0700

    commit

---
+++ codecs/avif/enc/avif_enc.d.ts
@@ -0,0 +1,24 @@
+export interface EncodeOptions {
+  cqLevel: number;
+  minQuantizerAlpha: number;
+  tileRowsLog2: number;
+  tileColsLog2: number;
+  speed: number;
+  subsample: number;
+  chromaDeltaQ: boolean;
+  sharpness: number;
+  targetSsim: boolean;
+}
+
+export interface AVIFModule extends EmscriptenWasm.Module {
+  encode(
+    data: BufferSource,
+    width: number,
+    height: number,
+    options: EncodeOptions,
+  ): Uint8Array | null;
+}
+
+declare var moduleFactory: EmscriptenWasm.ModuleFactory<AVIFModule>;
+
+export default moduleFactory;
---
+++ codecs/oxipng/pkg-parallel/squoosh_oxipng_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export function optimise(a: number, b: number, c: number, d: number): void;
+export function worker_initializer(a: number): number;
+export function start_main_thread(): void;
+export function start_worker_thread(): void;
+export const __wbindgen_export_0: WebAssembly.Memory;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_start(): void;
